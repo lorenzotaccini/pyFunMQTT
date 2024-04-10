1fcdd05a132055a9ae875f992c7847e4e15c05f7
lollo <lollo.taccini@gmail.com>
2024-04-10 16:21:48 +0200

    trying out some basic features, testing argparse and pyyaml

---
+++ Utils/CLI.py
@@ -0,0 +1,21 @@
+import docopt as doc
+import argparse
+
+
+class CLI:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="PyFunMQTT")
+        self.parser.add_argument("-c", "--configfile", default="config.yaml",
+                                 help="load config file in YAML format (default: config.yaml in Tests folder)")
+        self.parser.add_argument("-p", "--port",
+                                 help="connection port on which the broker is operating")
+        self.parser.add_argument("-b", "--broker", default="localhost",
+                                 help="broker's IP address (default: localhost)")
+        self.parser.add_argument("-t", "--topic", default="nomedefaulttopic",
+                                 help="topic on which you're listening (default: nomedefaulttopic)")
+        self.args = self.parser.parse_args()
+
+
+if __name__ == "__main__":
+    cli = CLI()
+    print(cli.args)
--- Utils/yaml_loader.py
+++ Utils/yaml_loader.py
@@ -1,4 +1,18 @@
+import yaml
 import yaml as y
+import CLI as cli
 
 
+class YamlLoader:
+    def __init__(self):
+        self.runargs = cli.CLI()
 
+
+with open("..\\Tests\\Test.yaml") as stream:
+    try:
+        print(yaml.safe_load("unbalanced blackets: ]["))
+    except yaml.YAMLError as exc:
+        if hasattr(exc, 'problem_mark'):
+            print(f"Error in YAML file {stream.name}, line {exc.problem_mark.line + 1}")
+        else:
+            print(exc)
