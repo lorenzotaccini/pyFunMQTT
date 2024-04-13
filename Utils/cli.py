e2944600e564cc1885971b02159a5d9f0e523db7
lollo <lollo.taccini@gmail.com>
2024-04-13 18:30:37 +0200

    created a function that checks correct config file's fields and their presence

--- Utils/cli.py
+++ Utils/cli.py
@@ -1,11 +1,13 @@
 
 import argparse
+import os
 
 
 class CLI:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="PyFunMQTT")
-        self.parser.add_argument("-c", "--configfile", default="Tests/config.yml",
+        self.parser.add_argument("-c", "--configfile",
+                                 default=str(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Tests', 'config.yml')),
                                  help="load config file in YAML format (default: config.yml in Tests folder)")
         self.parser.add_argument("-p", "--port",
                                  help="connection port on which the broker is operating")
@@ -14,7 +16,6 @@ class CLI:
         self.parser.add_argument("-t", "--topic", default="nomedefaulttopic",
                                  help="topic on which you're listening (default: nomedefaulttopic)")
         self.args = self.parser.parse_args()
-
 
 if __name__ == "__main__":
     cli = CLI()
--- Utils/yaml_loader.py
+++ Utils/yaml_loader.py
@@ -1,3 +1,5 @@
+import sys
+
 import yaml as y
 import cli as cli
 import inspect
@@ -24,17 +26,60 @@ class YamlLoader:
         except FileNotFoundError:
             print("config file not found, maybe incorrect path?")
 
+    # class method to check correct fields spelling and presence
     @classmethod
     def check_structure(cls, yaml_content: dict) -> bool:
-        topic_matcher = r"^([a-zA-Z0-9_-]+/?)*[a-zA-Z0-9_-]+$"
+        topic_matcher = r"^([a-zA-Z0-9_\-#]+/?)*[a-zA-Z0-9_\-#]+$"
+        function_matcher = r"^([a-zA-Z0-9_\-])+$"
+        key_matcher = r"^([a-zA-Z0-9_\-])+$"
+        out_format_matcher = ['json', 'yaml', 'xml']
 
-        if re.fullmatch(topic_matcher, yaml_content['topic']):
-            print("topic correct")
+        # catches and enlightens missing keys from YAML file
+        try:
+            # check input topic
+            if re.fullmatch(topic_matcher, yaml_content['topic']):
+                print("input topic correct")
+            else:
+                print("wrong format for input topic", file=sys.stderr)
+                return False
+
+            # check output topic
+            if re.fullmatch(topic_matcher, yaml_content['outTopic']):
+                print("output topic correct")
+            else:
+                print("wrong format for output topic", file=sys.stderr)
+                return False
+
+            # check function names
+            for i, f in enumerate(yaml_content['function']):
+                if re.fullmatch(function_matcher, f):
+                    print(f"function {i} correct")
+                else:
+                    print(f"wrong format for function: {f}", file=sys.stderr)
+                    return False
+
+            # check for correct output format
+            if yaml_content['outFormat'] in out_format_matcher:
+                print("output format allowed")
+            else:
+                print("wrong output format specified", file=sys.stderr)
+                return False
+
+            # check for optional key value, False not returned in that case
+            if 'key' in yaml_content:  # key field is optional
+                if re.fullmatch(key_matcher, yaml_content['key']):
+                    print("key correct")
+                else:
+                    print("wrong format for key", file=sys.stderr)
+            else:
+                print("key field not present")
+        except KeyError as k:
+            print(f"some required keys are missing from YAML config file: {k.args}", file=sys.stderr)
+        # all checked, return True
         return True
 
 
 if __name__ == "__main__":
     yl = YamlLoader()
     confstruct = yl.load()
-    print(confstruct)
     YamlLoader.check_structure(confstruct)
