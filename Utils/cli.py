d8cdae1d1aec597e2da13c45354b5686bf660348
lollo <lollo.taccini@gmail.com>
2024-05-21 20:21:25 +0200

    transition from print to custom logging is complete. Implementing silent mode

--- Utils/cli.py
+++ Utils/cli.py
@@ -5,20 +5,31 @@ import os
 class CLI:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="PyFunMQTT")
+
+        silent_verbose_group = self.parser.add_mutually_exclusive_group()
+
         self.parser.add_argument("-c", "--configfile",
                                  default=str(
                                      os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Tests',
                                                   'config.yml')),
                                  help="load config file in YAML format (default: config.yml in Tests folder)")
-        self.parser.add_argument("-v", "--verbose", default=True, action='store_true', help='verbose mode, allows to '
-                                                                                            'see all traffic'
-                                                                                            'being processed by the '
-                                                                                            'clients')
-        self.parser.add_argument('-s', '--silent', default=False, action='store_true', help='disable all stdout and '
-                                                                                            'stderr logging. File '
-                                                                                            'logging is still enabled')
-        self.parser.add_argument("-w", default=10, help="wait time in seconds between watchdog scans on given "
-                                                        "configuration file")
+        silent_verbose_group.add_argument("-v", "--verbose",
+                                          default=False,
+                                          action='store_true',
+                                          help='verbose mode, allows to see all the traffic ')
+        silent_verbose_group.add_argument('-s',
+                                          '--silent',
+                                          default=False,
+                                          action='store_true',
+                                          help='disable all stdout and stderr logging. File logging is still enabled')
+        self.parser.add_argument('--log-messages',
+                                 default=False,
+                                 action='store_true',
+                                 help='enable logging messages on separate log file. Note that program\'s performance '
+                                      'will be negatively affected by this')
+        self.parser.add_argument("-w",
+                                 default=10,
+                                 help="wait time in seconds between watchdog scans on given configuration file")
         self.args = self.parser.parse_args()
 
 
--- pyFunMQTT.py
+++ pyFunMQTT.py
@@ -1,13 +1,28 @@
+import os
+
 import Worker.core as core
 import Utils.cli as cli
 
 import logging
 import sys
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 
 
-def setup_logger(verbose):
+def setup_logger(verbose, silent, logmessages):
     logger = logging.getLogger()
+
+    '''
+    # add level MESSAGE to the logger, his numeric value will be 5, the lowest
+    msg_log_lvl = 5
+    
+    def message(self, msg, *args, **kws):
+        if self.isEnabledFor(msg_log_lvl):
+            self._log(msg_log_lvl, msg, args, **kws)
+
+    logging.addLevelName(msg_log_lvl, "MESSAGE")
+    logging.Logger.message = message
+    '''
+
     logger.setLevel(logging.DEBUG)  # setup logging level
 
     formatter = logging.Formatter(
@@ -15,39 +30,43 @@ def setup_logger(verbose):
         datefmt='%d-%m-%Y %H:%M:%S'
     )
 
-    # all messages below WARNING level are logged on stdout
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.addFilter(lambda record: record.levelno < logging.WARNING)
+    '''
+    if logmessages:
+        # create a handler for logging messages, with rotation and dimension for each file capped to 3MB
+        msgs_file_handler = RotatingFileHandler(
+            (os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Logs', 'msgs.log')),
+            maxBytes=3 * 1024 * 1024,
+            backupCount=3
+        )
+        msgs_file_handler.setLevel(logging.MESSAGE)
+        msgs_file_handler.addFilter(lambda record: record.levelno < logging.DEBUG)  # only log MESSAGE
+        msgs_file_handler.setFormatter(formatter)
+        logger.addHandler(msgs_file_handler)
+    '''
 
-    # all messages beyond INFO level are logged on stderr
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setLevel(logging.WARNING)
+    if not silent:
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(formatter)
+        if verbose:
+            stdout_handler.setLevel(logging.INFO)
+        else:
+            stdout_handler.setLevel(logging.WARNING)
+        logger.addHandler(stdout_handler)
 
-    # Crea un handler per il file di log con rotazione
-    file_handler = RotatingFileHandler(
-        'app.log', maxBytes=5 * 1024 * 1024, backupCount=3
+    logfile_handler = TimedRotatingFileHandler(
+        (os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Logs', 'session.log')),
+        when='M',
+        interval=1,  # new file everyday
+        backupCount=7  # keeps the backups for a week before rollover
     )
-
-    if verbose:
-        stdout_handler.setLevel(logging.DEBUG)
-        file_handler.setLevel(logging.DEBUG)
-    else:
-        stdout_handler.setLevel(logging.WARNING)
-        file_handler.setLevel(logging.WARNING)
-
-    file_handler.setFormatter(formatter)
-    stdout_handler.setFormatter(formatter)
-    stderr_handler.setFormatter(formatter)
-
-    # Aggiungi gli handler al logger
-    logger.addHandler(stdout_handler)
-    logger.addHandler(file_handler)
-    logger.addHandler(stderr_handler)
+    logfile_handler.suffix = '%d_%m_%Y'
+    logfile_handler.setFormatter(formatter)
+    logger.addHandler(logfile_handler)
 
 
 if __name__ == '__main__':
     run_args = cli.CLI()
-    setup_logger(run_args.args.verbose)
+    setup_logger(run_args.args.verbose, run_args.args.silent, run_args.args.log_messages)
 
     client_spawner = core.Spawner(run_args)
     client_spawner.spawn_all()
